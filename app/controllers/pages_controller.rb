14c773c5d3622f7c0c09c872439e027433163511
Milo WIssig <mwissig@gmail.com>
2019-02-16 20:45:41 -0500

    style and leaderboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,7 @@ class PagesController < ApplicationController
   def vote
     @paintings = Painting.order("RANDOM()").first(2)
   end
-
+def top
+    @paintings = Painting.all.order('points DESC').paginate(:page => params[:page], :per_page => 10)
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'top' => 'pages#top'
   get 'vote' => 'pages#vote'
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
